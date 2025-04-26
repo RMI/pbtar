83412b89841eb45b9207742cbde235dce55c7d54
CJ Yetman <cj@cjyetman.com>
2025-04-26 14:29:09 +0200

    improve comment

--- api/main.py
+++ api/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
     },
 )
 
-origins = ["http://localhost", "null"]  # this necessary for a request from a local file
+origins = ["http://localhost", "null"]  # "null" is necessary for a request from a local file
 
 app.add_middleware(
     CORSMiddleware,
