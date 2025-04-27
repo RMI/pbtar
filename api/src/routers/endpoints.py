bce43d0c2befa0d202c0973af812c70307c2d0a0
CJ Yetman <cj@cjyetman.com>
2025-04-27 02:03:43 +0200

    add a `/scenarios` route and update frontend to load it by default

--- api/src/routers/endpoints.py
+++ api/src/routers/endpoints.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import inspect
+from sqlalchemy.orm import Session
+from models.sql_models import Scenario
 from services.db import get_db, engine
 
 
@@ -17,3 +19,12 @@ def get_tables_from_db():
     return inspector.get_table_names(
         schema="pbtar"
     )  # Get table names in the 'pbtar' schema
+
+
+@table_router.get("/scenarios")
+def get_scenarios(db: Session = Depends(get_db)):
+    # Query the scenarios
+    scenarios = db.query(Scenario).all()
+    if not scenarios:
+        raise HTTPException(status_code=404, detail="Scenario not found")
+    return scenarios
