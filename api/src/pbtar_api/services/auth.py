e7a63d388831228673d6d4ece2bb725ad44256e5
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-05-16 16:58:32 +0200

    Revert "accept API_KEY in url"
    This reverts commit 3a817a29850544f46c47540764be4b2ef05eed3c.

--- api/src/pbtar_api/services/auth.py
+++ api/src/pbtar_api/services/auth.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import HTTPException, Security, Depends, Request
+from fastapi import HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 from dotenv import load_dotenv
 
@@ -12,17 +12,7 @@ API_KEY_NAME = "X-API-Key"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
 
-def get_api_key(
-    request: Request,
-    api_key_header: str = Security(api_key_header)
-):
-    # Check header first (for backward compatibility)
-    if api_key_header and api_key_header == API_KEY:
-        return api_key_header
-        
-    # Then check query parameter
-    api_key_query = request.query_params.get("api_key")
-    if api_key_query and api_key_query == API_KEY:
-        return api_key_query
-        
+def get_api_key(api_key: str = Security(api_key_header)):
+    if api_key == API_KEY:
+        return api_key
     raise HTTPException(status_code=403, detail="Invalid API Key")
