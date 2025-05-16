1b28e3a4687dce2a00f7ec0f8adeef8745fb62f9
CJ Yetman <cj@cjyetman.com>
2025-05-16 14:49:41 +0200

    add integration tests

---
+++ test-integration.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+check_url()
+{
+  URL=$1
+  EXPECT=$2
+  RESPONSE_CODE=$( \
+  curl \
+    --silent \
+    --output /dev/null \
+    --write-out "%{http_code}" \
+    "$URL"
+  )
+
+  echo "$URL": "$RESPONSE_CODE" \(expected: "$EXPECT"\)
+  if [ "$RESPONSE_CODE" != "$EXPECT" ]; then exit $?; fi
+}
+
+check_url "http://localhost/" 307
+check_url "http://localhost/docs" 200
+check_url "http://localhost/docs/" 307
+check_url "http://localhost/health" 200
+check_url "http://localhost/health/" 307
+check_url "http://localhost/tables" 200
+check_url "http://localhost/tables/" 307
+check_url "http://localhost/scenarios" 200
+check_url "http://localhost/scenarios/" 307
+check_url "http://localhost/organizations" 200
+check_url "http://localhost/organizations/" 307
+check_url "http://localhost/xxx" 404
