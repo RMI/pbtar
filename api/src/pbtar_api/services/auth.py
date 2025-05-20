78d1ceffe4259761772c76fe4c8f159d6fef15f2
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-05-20 15:24:38 +0200

    revert another typo

--- api/src/pbtar_api/services/auth.py
+++ api/src/pbtar_api/services/auth.py
@@ -12,7 +12,7 @@ PBTAR_API_KEY_NAME = "X-API-Key"
 pbtar_api_key_header = APIKeyHeader(name=PBTAR_API_KEY_NAME, auto_error=True)
 
 
-def get_PBTAR_API_KEY(PBTAR_API_KEY: str = Security(pbtar_api_key_header)):
+def get_api_key(PBTAR_API_KEY: str = Security(pbtar_api_key_header)):
     if PBTAR_API_KEY == PBTAR_API_KEY:
         return PBTAR_API_KEY
     raise HTTPException(status_code=403, detail="Invalid API Key")
