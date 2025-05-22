1d89f05d41ce1a55ce60d62408c221de5d020013
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-05-22 17:38:46 +0200

    route to About Us page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="/about" element={<AboutPage />} />
           </Routes>
         </main>
         <Footer />
