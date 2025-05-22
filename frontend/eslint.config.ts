e0624dd7090e59cb02e458368a961e4016cba71e
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-05-22 11:31:30 +0200

    setup eslint config

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,8 +1,8 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -10,19 +10,11 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      react: reactPlugin,
     },
   }
 );
