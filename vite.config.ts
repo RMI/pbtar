9fc291f5129ee70aa0ec035649ed6ae13ce9d95c
Alex Axthelm <aaxthelm@RMI.org>
2025-08-11 19:11:18 +0200

    Remove unneeded rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -117,13 +117,4 @@ export default defineConfig({
     open: true,
     port: 3000,
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        schema: "pbtar_schema.json",
-      },
-    },
-  },
-  publicDir: "public",
 });
