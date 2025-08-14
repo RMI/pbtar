370771f8082589a607a03f66e288d5513c890350
Alex Axthelm <aaxthelm@RMI.org>
2025-08-14 15:43:32 +0200

    Generate scheam docs in script

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -6,4 +6,10 @@ python3 -m venv ".venv/python"
 # This adds the generate-schema-doc command
 pip3 install "json-schema-for-humans"
 
+generate-schema-doc \
+  --config-file scripts/schema-render-config.json \
+  public/ \
+  public/ 
+
+npm run format
 
