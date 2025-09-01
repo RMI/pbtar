94f2056117f1c244203dec3e4b47b545c5c822fc
CJ Yetman <cj@cjyetman.com>
2025-09-01 13:09:12 +0200

    enable Markdown in Data Source section of details page

--- src/pages/ScenarioDetailPage.tsx
+++ src/pages/ScenarioDetailPage.tsx
@@ -138,9 +138,7 @@ const ScenarioDetailPage: React.FC = () => {
                   Data Source
                 </h2>
                 <div className="bg-neutral-50 border border-neutral-200 rounded-lg p-4">
-                  <p className="text-rmigray-700 mb-4">
-                    {scenario.dataSource.description}
-                  </p>
+                  <ReactMarkdown>{scenario.dataSource.description}</ReactMarkdown>
                   <div className="flex flex-col sm:flex-row gap-3">
                     <a
                       href={scenario.dataSource.url}
