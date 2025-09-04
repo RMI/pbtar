1a42a63ada87ff2df222464e9d0ece45754e815d
Alex Axthelm <aaxthelm@RMI.org>
2025-09-04 17:44:54 +0200

    chore(tooltip): clean tooltip strings (end in `.`)

--- src/utils/tooltipUtils.ts
+++ src/utils/tooltipUtils.ts
@@ -10,19 +10,19 @@ export const pathwayTypeTooltips: Record<PathwayType, string> = {
 };
 
 export const sectorTooltips: Record<Sector, string> = {
-  "Agriculture": "Agricultural activities",
+  "Agriculture": "Agricultural activities.",
   "Automotive": "Automotive manufacturing.",
   "Aviation": "Logistics of passengers and cargo by airplane.",
   "Buildings": "Residential and commercial buildings. Focus on energy use.",
-  "Cement": "Cement manufacturing",
+  "Cement": "Cement manufacturing.",
   "Chemicals":
-    "Production of primary chemicals and/or chemicals for end use, such as plastics, fertilizer, pharmaceuticals",
-  "Coal Mining": "Extraction of coal",
-  "Gas (Upstream)": "Extraction of natural gas",
+    "Production of primary chemicals and/or chemicals for end use, such as plastics, fertilizer, pharmaceuticals.",
+  "Coal Mining": "Extraction of coal.",
+  "Gas (Upstream)": "Extraction of natural gas.",
   "Industry":
-    "Focused on manufacturing activities, especially heavy industries and hard-to-abate industries",
-  "Land Use": "Agriculture, Forestry, Fishery, other forms of land use",
-  "Oil (Upstream)": "Extraction of oil",
+    "Focused on manufacturing activities, especially heavy industries and hard-to-abate industries.",
+  "Land Use": "Agriculture, Forestry, Fishery, other forms of land use.",
+  "Oil (Upstream)": "Extraction of oil.",
   "Other":
     "Other climate relevant sectors that are not covered by any of the available categories.",
   "Power":
@@ -33,7 +33,7 @@ export const sectorTooltips: Record<Sector, string> = {
   "Shipping": "Logistics of passengers and cargo by ship.",
   "Steel":
     "Steel making, both primary and secondary. Can include upstream and downstream activities.",
-  "Transport": "Logistics of passengers and cargo",
+  "Transport": "Logistics of passengers and cargo.",
 };
 
 const unknownTooltip = "No tooltip available";
