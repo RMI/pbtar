b7576abd81bbf706469bc31f5594367b524d05b6
CJ Yetman <cj@cjyetman.com>
2025-10-27 22:08:38 +0100

    donut chart clean-up and transitions

--- src/components/DonutChart.tsx
+++ src/components/DonutChart.tsx
@@ -3,54 +3,67 @@ import { useRef, useEffect, useState } from "react";
 
 export default function DonutChart({
   data,
-  width = 600,
+  width = 400,
   marginTop = 20,
   marginRight = 20,
   marginBottom = 20,
   marginLeft = 20,
 }) {
   const [d3data, setD3data] = useState(
-    data.data.filter((d) => (d.sector == "Power") & (d.metric == "Capacity") & (d.year == 2022)),
+    data.data.filter(d => (d.sector == "Power") & (d.metric == "Capacity") & (d.year == 2022)),
   );
   const ref = useRef();
 
-  const height = Math.min(500, width / 2);
+  const height = Math.min(500, width);
+  const outerRadius = height / 2 - 10;
+  const innerRadius = outerRadius * 0.5;
+
+  const tau = 2 * Math.PI;
+  const color = d3.scaleOrdinal(d3.schemeObservable10);
+
+  const arc = d3.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
+
+  const pie = d3.pie().sort(null).value(d => d.value);
 
   useEffect(() => {
     const svgElement = d3.select(ref.current);
 
-    const outerRadius = (height) / 2 - 10;
-    const innerRadius = outerRadius * 0.40;
-    const tau = 2 * Math.PI;
-    const color = d3.scaleOrdinal(d3.schemeObservable10);
-
-    const arc = d3.arc()
-      .innerRadius(innerRadius)
-      .outerRadius(outerRadius);
-
-    const pie = d3.pie().sort(null).value((d) => d.value);
-
     svgElement.attr("viewBox", [-width/2, -height/2, width, height]);
 
-    svgElement.datum(d3data).selectAll("path")
+    const path = svgElement.datum(d3data).selectAll("path")
       .data(pie)
       .join("path")
         .attr("fill", (d, i) => color(i))
-        .attr("d", arc)
-        .attr("data-year", (d) => d.year)
-        .attr("data-value", (d) => d.value)
-        .attr("data-technology", (d) => d.technology)
-        .each(function(d) { this._current = d; }); // store the initial angles
+        .attr("data-year", d => d.data.year )
+        .attr("data-value", d => d.data.value)
+        .attr("data-technology", d => d.data.technology)
+        .transition()
+        .duration(750)
+        .attrTween("d", arcTween);
   }, [d3data]);
 
-  const filterData = (selectedTech) =>
+  function arcTween(a) {
+    const i = d3.interpolate(this._current, a);
+    this._current = i(0);
+    return (t) => arc(i(t));
+  }
+
+  function uniqueYears(data) {
+    return data.data
+      .reduce((a, b) => a.indexOf(b.year) < 0 ? a.concat([b.year]) : a, [])
+      .sort()
+  }
+
+  const filterData = (selectedYear) =>
     setD3data(
       data.data.filter(
-        (d) =>
-          (d.sector == "Power") &
-          (d.technology == selectedTech) &
-          (d.metric == "Capacity"),
-      ),
+        d =>
+          d.sector == "Power" &
+          d.metric == "Capacity" &
+          d.year == selectedYear
+      )
     );
 
   return (
@@ -58,10 +71,8 @@ export default function DonutChart({
       <label>
         Year:
         <select onChange={(e) => filterData(e.target.value)}>
-          {d3data && d3data.reduce(function(a, b) { return (a.indexOf(b.year) < 0) ? a.concat([b.year]) : a; }, []).sort().map(e => (
-            <>
-              <option value = {e}>{e}</option>
-            < />
+          {data && uniqueYears(data).map(e => (
+            <option value={e}>{e}</option>
           ))}
         </select>
       </label>
