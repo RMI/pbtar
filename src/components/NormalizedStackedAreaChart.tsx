2c5930b743dbcac220555bfd4bc8a4679c5dae63
CJ Yetman <cj@cjyetman.com>
2025-10-28 02:48:05 +0100

    basic normalized stacked area chart

--- src/components/NormalizedStackedAreaChart.tsx
+++ src/components/NormalizedStackedAreaChart.tsx
@@ -3,7 +3,7 @@ import { useRef, useEffect, useState } from "react";
 
 export default function NormalizedStackedAreaChart({
   data,
-  width = 640,
+  width = 600,
   height = 400,
   marginTop = 20,
   marginRight = 20,
@@ -11,92 +11,58 @@ export default function NormalizedStackedAreaChart({
   marginLeft = 40,
 }) {
   const [d3data, setD3data] = useState(
-    data.data.filter((d) => (d.sector == "Power") & (d.metric == "Capacity")),
+    data.data.filter(d => (d.sector == "Power") & (d.metric == "Capacity"))
   );
   const ref = useRef();
   const gx = useRef();
   const gy = useRef();
-  const lines = useRef();
-  const dots = useRef();
+  const areas = useRef();
 
   useEffect(() => {
     const svgElement = d3.select(ref.current);
-    const linesGroup = d3.select(lines.current);
-    const dotsGroup = d3.select(dots.current);
+    const areasGroup = d3.select(areas.current);
 
     const utc = d3.utcParse("%Y");
-    const years = d3.extent(d3data, (d) => utc(d.year));
-    const values = d3.extent(d3data, (d) => d.value);
-    const xticks = [...new Set(d3data.map((d) => d.year))].map(utc);
+    const years = d3.extent(d3data, d => utc(d.year));
+    const values = d3.extent(d3data, d => d.value);
+    const xticks = [...new Set(d3data.map(d => d.year))].map(utc);
 
     const x = d3.scaleUtc(years, [marginLeft, width - marginRight]);
-    const y = d3.scaleLinear(values, [height - marginBottom, marginTop]);
-    const line = d3
-      .line()
-      .x((d) => x(utc(d.year)))
-      .y((d) => y(d.value));
+
+    const y = d3.scaleLinear().rangeRound([height - marginBottom, marginTop]);
+
+    const series = d3.stack()
+      .offset(d3.stackOffsetExpand)
+      .keys(d3.union(d3data.map(d => d.technology)))
+      .value(([, D], key) => D.get(key).value)
+    (d3.index(d3data, d => d.year, d => d.technology));
+
+    const color = d3.scaleOrdinal()
+      .domain(series.map(d => d.technology))
+      .range(d3.schemeTableau10);
+
+    const area = d3.area()
+      .x(d => x(utc(d.data[0])))
+      .y0(d => y(d[0]))
+      .y1(d => y(d[1]));
 
     d3.select(gx.current)
       .transition()
       .duration(750)
       .call(d3.axisBottom(x).tickValues(xticks));
-    d3.select(gy.current).transition().duration(750).call(d3.axisLeft(y));
 
-    const groupedData = d3.groups(d3data, (d) => d.technology);
-    const groupNames = groupedData.map((d) => d[0]);
-    const groupColor = d3
-      .scaleOrdinal()
-      .domain(["Biomass", "Wind", "Hydro", "Solar", "Oil", "Coal", "Gas"])
-      .range([
-        "var(--color-slate)",
-        "var(--color-pine)",
-        "var(--color-donate)",
-        "var(--color-solar)",
-        "var(--color-deficient)",
-        "var(--color-coal)",
-        "var(--color-calm)",
-      ]);
+    d3.select(gy.current)
+      .transition()
+      .duration(750)
+      .call(d3.axisLeft(y).ticks(5, "%"));
 
-    linesGroup
-      .selectAll(".line")
-      .data(groupedData)
+    areasGroup
+      .selectAll()
+      .data(series)
       .join("path")
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "currentColor")
-      .attr("stroke", (d) => groupColor(d[0]))
-      .attr("stroke-width", 1.5)
-      .attr("d", (d) => line(d[1]));
-
-    dotsGroup
-      .selectAll("circle")
-      .data(d3data)
-      .join("circle")
-      .attr("key", (d, i) => i)
-      .attr("cx", (d) => x(utc(d.year)))
-      .attr("cy", (d) => y(d.value))
-      .attr("fill", (d) => groupColor(d.technology))
-      .attr("stroke", (d) => groupColor(d.technology))
-      .attr("stroke-width", 1.5)
-      .attr("data-year", (d) => d.year)
-      .attr("data-value", (d) => d.value)
-      .attr("data-geography", (d) => d.geography)
-      .attr("data-metric", (d) => d.metric)
-      .attr("data-sector", (d) => d.sector)
-      .attr("data-technology", (d) => d.technology)
-      .attr("data-unit", (d) => d.unit)
-      .attr("r", 2.5);
+        .attr("fill", (d, i) => color(i))
+        .attr("d", d => area(d));
   }, [d3data]);
-
-  const filterData = (selectedTech) =>
-    setD3data(
-      data.data.filter(
-        (d) =>
-          (d.sector == "Power") &
-          (d.technology == selectedTech) &
-          (d.metric == "Capacity"),
-      ),
-    );
 
   return (
     <>
@@ -113,8 +79,7 @@ export default function NormalizedStackedAreaChart({
           ref={gy}
           transform={`translate(${marginLeft}, 0)`}
         />
-        <g ref={lines} />
-        <g ref={dots} />
+        <g ref={areas} />
       </svg>
     </>
   );
