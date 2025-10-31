f9eeebf64086377c5e915124f51ac4d9957661fe
CJ Yetman <cj@cjyetman.com>
2025-10-31 18:03:01 +0100

    basic radar plot

--- src/components/RadarChart.tsx
+++ src/components/RadarChart.tsx
@@ -3,99 +3,120 @@ import { useRef, useEffect, useState } from "react";
 
 export default function RadarChart({
   data,
-  width = 640,
-  height = 400,
-  marginTop = 20,
-  marginRight = 20,
-  marginBottom = 30,
-  marginLeft = 40,
+  width = 400,
+  margin = 20,
 }) {
   const [d3data, setD3data] = useState(
-    data.data.filter((d) => (d.sector == "Power") & (d.metric == "Capacity")),
+    data.data.filter(d => (d.sector == "power") & (d.metric == "capacity") & (d.year == 2022)),
   );
   const ref = useRef();
-  const gx = useRef();
-  const gy = useRef();
-  const lines = useRef();
-  const dots = useRef();
+
+  const height = width;
+  const containerWidth = width - (margin * 2);
+  const containerHeight = height - (margin * 2);
 
   useEffect(() => {
-    const svgElement = d3.select(ref.current);
-    const linesGroup = d3.select(lines.current);
-    const dotsGroup = d3.select(dots.current);
+window.d3 = d3; window.d3data = d3data;
+    const svg = d3.select(ref.current)
+      .attr("width", width)
+      .attr("height", height);
 
-    const utc = d3.utcParse("%Y");
-    const years = d3.extent(d3data, (d) => utc(d.year));
-    const values = d3.extent(d3data, (d) => d.value);
-    const xticks = [...new Set(d3data.map((d) => d.year))].map(utc);
+    const axisLabelFactor = 1.12;
+    const axisCircles = 5;
+    const dotRadius = 5;
+    const radius = (width / 2) - (2 * margin);
+    const axesDomain = Array.from(d3.union(d3data.map(d => d.technology)));
+    const maxValue = d3.max(d3data, d => d.value);
+    const angleSlice = Math.PI / 2 / axesDomain.length * 4;
+    const axisColor = "#CDCDCD";
 
-    const x = d3.scaleUtc(years, [marginLeft, width - marginRight]);
-    const y = d3.scaleLinear(values, [height - marginBottom, marginTop]);
-    const line = d3
-      .line()
-      .x((d) => x(utc(d.year)))
-      .y((d) => y(d.value));
+    const radarLine = d3.lineRadial()
+      .curve(d3.curveLinearClosed)
+      .radius(d => rScale(d))
+      .angle((d, i) => i * angleSlice);
 
-    d3.select(gx.current)
-      .transition()
-      .duration(750)
-      .call(d3.axisBottom(x).tickValues(xticks));
-    d3.select(gy.current).transition().duration(750).call(d3.axisLeft(y));
+    const rScale = d3.scaleLinear()
+      .domain(d3.extent(d3data, d => d.value))
+      .range([0, radius]);
 
-    const groupedData = d3.groups(d3data, (d) => d.technology);
-    const groupNames = groupedData.map((d) => d[0]);
-    const groupColor = d3
-      .scaleOrdinal()
-      .domain(["Biomass", "Wind", "Hydro", "Solar", "Oil", "Coal", "Gas"])
-      .range([
-        "var(--color-slate)",
-        "var(--color-pine)",
-        "var(--color-donate)",
-        "var(--color-solar)",
-        "var(--color-deficient)",
-        "var(--color-coal)",
-        "var(--color-calm)",
-      ]);
+    const container = svg.append('g')
+      .attr("width", containerWidth)
+      .attr("height", containerHeight)
+      .attr('transform', 'translate(' + ((width / 2) + margin) + ',' + ((height / 2) + margin) + ')');
 
-    linesGroup
-      .selectAll(".line")
-      .data(groupedData)
-      .join("path")
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "currentColor")
-      .attr("stroke", (d) => groupColor(d[0]))
-      .attr("stroke-width", 1.5)
-      .attr("d", (d) => line(d[1]));
+  	var axisGrid = container.append("g")
+      .attr("class", "axisWrapper");
 
-    dotsGroup
-      .selectAll("circle")
+  	axisGrid.selectAll(".levels")
+  	   .data(d3.range(1, axisCircles + 1).reverse())
+  	   .enter()
+        .append("circle")
+        .attr("class", "gridCircle")
+        .attr("r", d => radius / axisCircles * d)
+        .style("fill", "none")
+        .style("stroke", axisColor);
+
+  	const axis = axisGrid.selectAll(".axis")
+  		.data(axesDomain)
+  		.enter()
+        .append("g")
+        .attr("class", "axis");
+
+  	axis.append("line")
+  		.attr("x1", 0)
+  		.attr("y1", 0)
+  		.attr("x2", (d, i) => rScale(maxValue*1.05) * Math.cos(angleSlice*i - Math.PI/2))
+  		.attr("y2", (d, i) => rScale(maxValue*1.05) * Math.sin(angleSlice*i - Math.PI/2))
+  		.attr("class", "line")
+  		.style("stroke", axisColor)
+  		.style("stroke-width", "2px");
+
+  	axis.append("text")
+  		.attr("class", "legend")
+  		.style("font-size", "11px")
+  		.attr("text-anchor", "middle")
+      .attr("dy", "0.35em")
+  		.attr("x", (d, i) => rScale(maxValue * axisLabelFactor) * Math.cos(angleSlice*i - Math.PI/2))
+  		.attr("y", (d, i) => rScale(maxValue * axisLabelFactor) * Math.sin(angleSlice*i - Math.PI/2))
+  		.text(d => d);
+
+    const plots = container.append('g')
+      .selectAll('g')
       .data(d3data)
+      .join('g')
+        .attr("fill", "none")
+        .attr("stroke", "steelblue");
+
+    plots.append('path')
+      .attr("d", d => radarLine(d3data.map(d => d.value)))
+      .attr("fill", "steelblue")
+      .attr("fill-opacity", 0.1)
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2);
+
+  	plots.selectAll("circle")
+  		.data(d => d)
       .join("circle")
-      .attr("key", (d, i) => i)
-      .attr("cx", (d) => x(utc(d.year)))
-      .attr("cy", (d) => y(d.value))
-      .attr("fill", (d) => groupColor(d.technology))
-      .attr("stroke", (d) => groupColor(d.technology))
-      .attr("stroke-width", 1.5)
-      .attr("data-year", (d) => d.year)
-      .attr("data-value", (d) => d.value)
-      .attr("data-geography", (d) => d.geography)
-      .attr("data-metric", (d) => d.metric)
-      .attr("data-sector", (d) => d.sector)
-      .attr("data-technology", (d) => d.technology)
-      .attr("data-unit", (d) => d.unit)
-      .attr("r", 2.5);
+  		  .attr("r", dotRadius)
+  		  .attr("cx", (d,i) => rScale(d.value) * Math.cos(angleSlice*i - Math.PI/2))
+  		  .attr("cy", (d,i) => rScale(d.value) * Math.sin(angleSlice*i - Math.PI/2))
+  		  .style("fill-opacity", 0.8);
   }, [d3data]);
 
-  const filterData = (selectedTech) =>
+  function uniqueYears(data) {
+    return data.data
+      .reduce((a, b) => a.indexOf(b.year) < 0 ? a.concat([b.year]) : a, [])
+      .sort()
+  }
+
+  const filterData = (selectedYear) =>
     setD3data(
       data.data.filter(
-        (d) =>
-          (d.sector == "Power") &
-          (d.technology == selectedTech) &
-          (d.metric == "Capacity"),
-      ),
+        d =>
+          d.sector == "power" &
+          d.metric == "capacity" &
+          d.year == selectedYear
+      )
     );
 
   return (
@@ -103,28 +124,14 @@ export default function RadarChart({
       <label>
         Year:
         <select onChange={(e) => filterData(e.target.value)}>
-          {d3data && d3data.reduce(function(a, b) { return (a.indexOf(b.year) < 0) ? a.concat([b.year]) : a; }, []).sort().map(e => (
-            <>
-              <option value = {e}>{e}</option>
-            < />
+          {data && uniqueYears(data).map(e => (
+            <option value={e}>{e}</option>
           ))}
         </select>
       </label>
       <svg
         ref={ref}
-        width={width}
-        height={height}
       >
-        <g
-          ref={gx}
-          transform={`translate(0, ${height - marginBottom})`}
-        />
-        <g
-          ref={gy}
-          transform={`translate(${marginLeft}, 0)`}
-        />
-        <g ref={lines} />
-        <g ref={dots} />
       </svg>
     </>
   );
